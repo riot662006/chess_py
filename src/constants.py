ef4c8e60a4761dfdcc147b9cbbf58d36ce55cce9
riot662006 <72505429+riot662006@users.noreply.github.com>
2024-04-30 01:18:11 -0500

    useful colors added for referencing

--- src/constants.py
+++ src/constants.py
@@ -3,4 +3,10 @@ SCREEN_HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+CLR_CHECK = "#FF0000"
+CLR_CHECKMATE = "#CC0000AA"
+CLR_CAPTURABLE = "#FC716B"
+CLR_SELECTED = "#ADF9A1"
+CLR_LAST_MOVE = "#F1EBBE"
+
 start_config = "rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR"
